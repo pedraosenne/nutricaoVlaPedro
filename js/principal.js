19e9982b74c0bbddd855e527f408f7b9b6841e9e
pedro_senne <pedro.senne.santos@escola.pr.gov.br>
2025-10-03 11:02:31 -0300

    sdv

--- js/principal.js
+++ js/principal.js
@@ -2,3 +2,9 @@ var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent = ("Aparecida Nutricionista");
+
+ var paciente = document.querySelector("#primeiro-paciente")
+ console.log(paciente)
+ var tdPeso = paciente.querySelector(".info-peso");
+ console.log(paciente);// tr
+console.log (tdPeso); //  td que tem o peso 
