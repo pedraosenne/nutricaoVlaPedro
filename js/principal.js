61791517bfd48c94c27f2d9b19438f4cfe02123f
pedro_senne <pedro.senne.santos@escola.pr.gov.br>
2025-10-21 09:45:33 -0300

    sdfg

--- js/principal.js
+++ js/principal.js
@@ -1,46 +1,51 @@
 var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
-titulo.textContent = ("White nutricao");
+titulo.textContent = ("nickin nutricao");
 
-var paciente = paciente.querySelectorAll(".paciente");
+
+var pacientes = paciente.querySelectorAll(".paciente");
+
 
 for (var i = 0; i < pacientes.length; i++) {
-    
 
-    var pacientes = pacientes1[1];
+    var paciente = pacientes[i];
+};
 
-var tdPeso = paciente.querySelector(".info-peso")
+
+var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
+
 
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
+
 var tdImc = paciente.querySelector(".info-imc");
+
 
 var pesoEhValido = true;
 var alturaEhValida = true;
 
-if (peso <= 0 || peso >= 1000) {
+
+if (peso <= 0 || peso > 1000) {
     console.log("Peso inválido");
     pesoEhValido = false;
     tdImc.textContent = "Peso inválido!";
 }
 
-if (altura <= 3.00 || altura >= 0) {
+
+if (altura > 3.00 || altura < 0) {
     console.log("Altura inválida");
     alturaEhValida = false;
     tdImc.textContent = "Altura inválida!";
 }
 
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
 
 if (alturaEhValida && pesoEhValido) {
     var imc = peso / (altura * altura);
     tdImc.textContent = imc;
 }
-}
+
+
 
