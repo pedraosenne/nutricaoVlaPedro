d6a49ec142e399c3a224f9c89333183b032ebc5d
pedro_senne <pedro.senne.santos@escola.pr.gov.br>
2025-10-21 09:50:22 -0300

    3QR

--- js/principal.js
+++ js/principal.js
@@ -10,39 +10,39 @@ for (var i = 0; i < pacientes.length; i++) {
     var paciente = pacientes[i];
 
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
 
-if (peso <= 0 || peso > 1000) {
-    console.log("Peso inválido");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido!";
-}
+    if (peso <= 0 || peso <= 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!";
+    }
 
 
-if (altura > 3.00 || altura < 0) {
-    console.log("Altura inválida");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida!";
-}
+    if (altura <= 3.00 || altura <= 0) {
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!";
+    }
 
 
-if (alturaEhValida && pesoEhValido) {
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
+    if (alturaEhValida && pesoEhValido) {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc;
+    }
 };
 
 
